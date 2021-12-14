ddf4f267c1677e1c5139ec5bf21687ecb221dac0
dlakswk <aksen78955@naver.com>
2021-12-14 21:26:27 +0900

    2021_12_14 캡스톤 2학기 기말고사 제출합니다.

--- app/src/main/java/com/example/a11_application/MainActivity.java
+++ app/src/main/java/com/example/a11_application/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -20,12 +24,14 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
     TextView textView;
+    DatePickerDialog.OnDateSetListener callbackMethon;
 
     static RequestQueue requestQueue;
 
@@ -37,28 +43,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.InitalLizeListenner();
+
+
         editText = findViewById(R.id.xeditText);
         textView = findViewById(R.id.textView);
 
         Button button = findViewById(R.id.xbutton);
-        button.setOnClickListener(new View.OnClickListener(){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){makeRequest();
+            public void onClick(View v) {
+                makeRequest();
+            }
+        });
+        Button dateButton = findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataProcess(v);
             }
         });
 
-        if(requestQueue == null){
+        if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(getApplicationContext());
         }
         recyclerView = findViewById(R.id.recyclerView);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(layoutManager);
 
         adapter = new MovieAdapter();
         recyclerView.setAdapter(adapter);
 
     }
-    public void makeRequest(){
+
+    public void InitalLizeListenner() {
+        callbackMethon = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                String year1 = String.valueOf(year);
+                String month1 = String.valueOf(month);
+                String dayOfMonth1 = String.valueOf(dayOfMonth);
+                String date = year1 + month1 + dayOfMonth1;
+                editText.setText("https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=ab9c2dcf5097608c8f8cf078b67c7046&targetDt=" + date);
+            }
+        };
+    }
+
+    public void dataProcess(View v) {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethon, 2021, 11, 20);
+        dialog.show();
+    }
+
+    public void makeRequest() {
         String url = editText.getText().toString();
         StringRequest request = new StringRequest(
                 Request.Method.GET,
@@ -76,10 +112,10 @@ public class MainActivity extends AppCompatActivity {
                         println("에러 -> " + error.getMessage());
                     }
                 }
-        ){
+        ) {
             @Override
-            protected Map<String,String> getParams() throws AuthFailureError{
-                Map<String, String> params = new HashMap<String,String>();
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
                 return params;
             }
         };
@@ -87,23 +123,24 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.add(request);
         println("요청 보냄");
     }
-    public void println(String data){
-        Log.d("MainActivity",data);
+
+    public void println(String data) {
+        Log.d("MainActivity", data);
     }
-    public void processResponse(String response)
-    {
+
+    public void processResponse(String response) {
         Gson gson = new Gson();
-        MovieList movieList = gson.fromJson(response,MovieList.class);
+        MovieList movieList = gson.fromJson(response, MovieList.class);
         println("영화 정보의 수 : " + movieList.boxOfficeResult.dailyBoxOfficeList.size());
 
-        for(int i=0; i<movieList.boxOfficeResult.dailyBoxOfficeList.size();i++)
-        {
+        for (int i = 0; i < movieList.boxOfficeResult.dailyBoxOfficeList.size(); i++) {
             Movie movie = movieList.boxOfficeResult.dailyBoxOfficeList.get(i);
             adapter.addItem(movie);
         }
         adapter.notifyDataSetChanged();
     }
 }
+
 
 
 
